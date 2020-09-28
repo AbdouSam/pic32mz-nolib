41ec7d011b7ce5a96a5f41e5460fc8655dcb197e
abdousam <abdou.sam7@gmail.com>
2020-09-28 09:25:10 +0100

    src/main.c Add and Fix UART read/write

--- src/main.c
+++ src/main.c
@@ -1,7 +1,16 @@
+/*
+ * @Author AbdouSam
+ * @brief, simplest code to test-run a PIC32MZ without any use of libraries
+ *
+ */
 #include <xc.h>
 
-/*** DEVCFG0 ***/
 
+// This define is to set the  µC to run on internal clock
+
+//#define CONFIG_CPU_USE_FRC
+
+/*** DEVCFG0 ***/
 #pragma config DEBUG =      OFF
 #pragma config JTAGEN =     OFF
 #pragma config ICESEL =     ICS_PGx1
@@ -69,82 +78,63 @@
 #pragma config PGL1WAY =    ON
 #pragma config PMDL1WAY =   ON
 #pragma config IOL1WAY =    ON
-#pragma config FUSBIDIO =   ON
+#pragma config FUSBIDIO =   OFF
 
 /*** BF1SEQ0 ***/
 
 #pragma config TSEQ =       0x0000
 #pragma config CSEQ =       0xffff
 
-#if 0
+#define SYS_FREQ 200000000 // 200MHz
 
-const DRV_USART_INIT drvUsart2InitData =
+#define MS_SCALE    (100000)  /* Calculated for a 200MHz system */
+/* Convert milliseconds to core timer ticks */
+#define MS2TICKS(T) ((T) * MS_SCALE) 
+
+/**
+ * @brief Read current core timer, used to create delays
+ *
+ * @Note This exactly same as the _CP0_SET_COUNT(c) and _CP0_GET_COUN()
+ * they set and get current tick from the co-processor in the mips processor.
+ */
+static uint32_t readcoretimer(void)
 {
-  .moduleInit.value     = DRV_USART_POWER_STATE_IDX2,
-  .usartID              = DRV_USART_PERIPHERAL_ID_IDX2,
-  .mode                 = DRV_USART_OPER_MODE_IDX2,
-  .flags                = DRV_USART_INIT_FLAGS_IDX2,
-  .brgClock             = DRV_USART_BRG_CLOCK_IDX2,
-  .lineControl          = DRV_USART_LINE_CNTRL_IDX2,
-  .baud                 = DRV_USART_BAUD_RATE_IDX2,
-  .handshake            = DRV_USART_HANDSHAKE_MODE_IDX2,
-  .interruptTransmit    = DRV_USART_XMIT_INT_SRC_IDX2,
-  .interruptReceive     = DRV_USART_RCV_INT_SRC_IDX2,
-  .interruptError       = DRV_USART_ERR_INT_SRC_IDX2,
-  .queueSizeTransmit    = DRV_USART_XMIT_QUEUE_SIZE_IDX2,
-  .queueSizeReceive     = DRV_USART_RCV_QUEUE_SIZE_IDX2,
-};
-
-const DRV_TMR_INIT drvTmr0InitData =
-{
-  .moduleInit.sys.powerState    = DRV_TMR_POWER_STATE_IDX0,
-  .tmrId                        = DRV_TMR_PERIPHERAL_ID_IDX0,
-  .clockSource                  = DRV_TMR_CLOCK_SOURCE_IDX0,
-  .prescale                     = DRV_TMR_PRESCALE_IDX0,
-  .mode                         = DRV_TMR_OPERATION_MODE_IDX0,
-  .interruptSource              = DRV_TMR_INTERRUPT_SOURCE_IDX0,
-  .asyncWriteEnable             = false,
-};
-
-void sys_init(void)
-{
-  printf("Init sys\n");
-
-  SYS_CLK_Initialize( NULL );
-  SYS_DEVCON_PerformanceConfig(SYS_CLK_SystemFrequencyGet());
-  SYS_DEVCON_JTAGEnable();
-  SYS_PORTS_Initialize();
-
-  /*** Interrupt Service Initialization Code ***/
-  SYS_INT_Initialize();
-
-  /* Initialize Drivers */
-
-  sysObj.drvTmr0 = DRV_TMR_Initialize(DRV_TMR_INDEX_0,
-                                      (SYS_MODULE_INIT*)&drvTmr0InitData);
-  SYS_INT_VectorPrioritySet(DRV_TMR_INTERRUPT_VECTOR_IDX0,
-                            DRV_TMR_INTERRUPT_PRIORITY_IDX0);
-  SYS_INT_VectorSubprioritySet(DRV_TMR_INTERRUPT_VECTOR_IDX0,
-                               DRV_TMR_INTERRUPT_SUB_PRIORITY_IDX0);
-
-
-  /* USART 0 Initialization */
-
-  sysObj.drvUsart2 = DRV_USART_Initialize(DRV_USART_INDEX_2,
-                                         (SYS_MODULE_INIT*)&drvUsart2InitData);
-  SYS_INT_VectorPrioritySet(INT_VECTOR_UART6_TX, DRV_USART_XMIT_INT_PRIORITY_IDX2);
-  SYS_INT_VectorSubprioritySet(INT_VECTOR_UART6_TX, DRV_USART_XMIT_INT_SUBPRIORITY_IDX2);
-  SYS_INT_VectorPrioritySet(INT_VECTOR_UART6_RX, DRV_USART_RCV_INT_PRIORITY_IDX2);
-  SYS_INT_VectorSubprioritySet(INT_VECTOR_UART6_RX, DRV_USART_RCV_INT_SUBPRIORITY_IDX2);
-  SYS_INT_VectorPrioritySet(INT_VECTOR_UART6_FAULT, DRV_USART_ERR_INT_PRIORITY_IDX2);
-  SYS_INT_VectorSubprioritySet(INT_VECTOR_UART6_FAULT, DRV_USART_ERR_INT_SUBPRIORITY_IDX2);
-
-
-  SYS_INT_Enable();
+  volatile uint32_t timer;
+  
+  asm volatile("mfc0   %0, $9" : "=r"(timer));
+  
+  return timer;
 }
 
-#endif
+static void setcoretime(volatile uint32_t count)
+{
+  asm volatile("mtc0    %0, $9" : :"r"(count));
+}
 
+/* Same as delay_us uses inline assembly for showing purposes */
+void delay_us_2(uint32_t us)
+{
+  us *= SYS_FREQ / 1000000 / 2; //Core timer updates every 2 ticks
+  setcoretime(0);
+
+  while (us > readcoretimer());
+}
+
+void __attribute__(("unused")) delay_us(uint32_t us)
+{
+  us *= SYS_FREQ / 1000000 / 2; //Core timer updates every 2 ticks
+
+  _CP0_SET_COUNT(0); // Set core timer count to 0
+
+  while (us > _CP0_GET_COUNT());
+}
+
+void delay_ms(uint32_t ms)
+{
+  delay_us_2(1000 * ms);
+}
+
+/* Set the clocks for different peripherals */
 void set_performance_mode()
 {
   unsigned int cp0;
@@ -159,7 +149,7 @@ void set_performance_mode()
   PB1DIVbits.PBDIV = 1; //Peripheral Bus 1 Clock Divisor Control (PBCLK1 is SYSCLK / 2)
   
   //PB2DIV
-  PB2DIVbits.PBDIV = 1; //Peripheral Bus 2 Clock Divisor Control (PBCLK1 is SYSCLK / 2)
+  PB2DIVbits.PBDIV = 1; //Peripheral Bus 2 Clock Divisor Control (PBCLK2 is SYSCLK / 2)
   PB2DIVbits.ON = 1; //Peripheral Bus 2 Output Clock Enable
 
   PB3DIVbits.PBDIV = 4;
@@ -194,20 +184,161 @@ void set_performance_mode()
 
 }
 
+/**
+ * UART Routines
+ */
+void _putc6(char c)
+{
+  while (U6STAbits.UTXBF);
+  U6TXREG = c;
+}
+
+void _putc4(char c)
+{
+  while (U4STAbits.UTXBF);
+  U4TXREG = c;
+}
+
+void print_str6(char *s)
+{
+  while ( *s != '\0')
+  {
+    _putc6(*s++);
+  }
+}
+
+
+void print_str4(char *s)
+{
+  LATASET = 1 << 10;
+  delay_ms(10);
+
+  while ( *s != '\0')
+  {
+    _putc4(*s++);
+  }
+
+  delay_ms(50);
+  LATACLR = 1 << 10;
+}
+
+void UART6_init(int baud_rate)
+{
+  int pb_uart_clk = SYS_FREQ / 2;
+
+  // Set up the UART 6
+  // RX to RPF2
+  U6RXR = 0b1011;
+  TRISFbits.TRISF2 = 1; //set its IO to input
+
+  // TX to RPF3
+  RPF3R = 0b0100;
+  TRISFbits.TRISF3 = 0; // Set its IO to output
+
+  U6MODE = 0; // Set UART 5 off, prior to setting it up.
+  U6MODEbits.BRGH = 0; // standard speed mode, included in the previsous statmem
+
+  // formulat straight from datasheet.
+  U6BRG = pb_uart_clk / ( 16  * baud_rate) - 1; 
+  U6STA = 0; // disable Tx Rx, clear all flags
+  U6STAbits.UTXEN = 1;
+  U6STAbits.URXEN = 1;
+
+  U6MODEbits.PDSEL = 0; // controls how many data bits parity,... we chose default
+  U6MODEbits.STSEL = 0; // stop bit we chose 1
+  
+  U6MODEbits.ON = 1; // Turn ON
+
+  print_str6("Hello UART \n");
+}
+
+
+void UART4_init(int baud_rate)
+{
+  int pb_uart_clk = SYS_FREQ / 2;
+
+  // Set rs485 bit to output
+  ANSELA = 0;
+  TRISACLR = 1 << 10;
+  // Set up the UART 6
+  // RX to RPF2
+  U4RXR = 0b0010;
+  TRISBbits.TRISB14 = 1; //set its IO to input
+
+  // TX to RPF3
+  RPF12R = 0b0010;
+
+  U4MODE = 0; // Set UART 5 off, prior to setting it up.
+  U4MODEbits.BRGH = 0; // standard speed mode, included in the previsous statmem
+
+  // formulat straight from datasheet.
+  U4BRG = pb_uart_clk / ( 16  * baud_rate) - 1; 
+  U4STA = 0; // disable Tx Rx, clear all flags
+  U4STAbits.UTXEN = 1;
+  U4STAbits.URXEN = 1;
+
+  U4MODEbits.PDSEL = 0; // controls how many data bits parity,... we chose default
+  U4MODEbits.STSEL = 0; // stop bit we chose 1
+  
+  U4MODEbits.ON = 1; // Turn ON
+
+  print_str4("Hello UART \n");
+}
+
+
+char read_char4(void)
+{
+  if (U4STAbits.URXDA) // Data ready
+  {
+    return U4RXREG;
+  }
+  return 0;
+}
+
 int main(void)
 {
 
   set_performance_mode();
 
   ANSELB = 0; // PIC starts all pins as analog, needs to be deactivated to be used
-  TRISBCLR = 1 << 13; //set bit 0 port A to output
-  TRISBCLR = 1 << 12; //set bit 0 port A to output
+  TRISBCLR = 1 << 13; //set bit 13 port B to output
+  TRISBCLR = 1 << 12; //set bit 12 port B to output
 
-  LATBSET = 1 <<  13; // Set bit 0 for Port A to 1
-  LATBSET = 1 <<  12; // Set bit 0 for Port A to 1
+  LATBSET = 1 <<  13; // Set bit 13 for Port B to 1
+  LATBSET = 1 <<  12; // Set bit 12 for Port B to 1
+
+  int delay_time = 1000;
+  char c;
+
+  UART4_init(19200);
 
   for (;; )
     {
+      delay_ms(delay_time);
+      LATBINV = 1 << 13;
+      LATBINV = 1 << 12;
+
+      c = 0;
+      c = read_char4();
+
+      if (c == 'u')
+      {
+        delay_time+=500;
+      }
+      else if (c == 'd')
+      {
+        delay_time-=500;
+      }
+
+      if (delay_time > 5000)
+      {
+        delay_time = 5000;
+      }
+      else if (delay_time < 500)
+      {
+        delay_time = 500;
+      }
+      print_str4("Hello UART \n");
     }
 
   return 0;

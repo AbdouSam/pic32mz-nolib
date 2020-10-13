1d671ea3c35c67da7396ca7ecf9bd2d7aa1e127d
abdousam <abdou.sam7@gmail.com>
2020-10-13 14:52:08 +0100

    src/error.h Fix spacing

--- src/error.h
+++ src/error.h
@@ -3,7 +3,8 @@
 
 #define OK     (0)
 #define ERR    (-1)
-#define ERRMAX (-2) //  out of range
+#define ERRMAX (-2) // out of range
 #define ERRNV  (-3) // not avaialble
 #define ERRVAL (-4) // wrong value
+
 #endif // MICROPY_INCLUDED_PIC32BIT_ERROR_H

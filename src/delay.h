077b35bfe4ccadc425f74a0372cb2143cec3d2b1
abdellah alaouchiche <abdou.sam7@gmail.com>
2021-02-28 15:29:35 +0100

    src/delay.* add comments to describe functions

--- src/delay.h
+++ src/delay.h
@@ -3,7 +3,14 @@
 
 #include <stdint.h>
 
+/*
+ * @brief generate a a blocking delay in us
+ */
 void delay_us(uint32_t us);
+
+/*
+ * @brief generate a a blocking delay in ms
+ */
 void delay_ms(uint32_t ms);
 
 #endif // MICROPY_INCLUDED_PIC32BIT_DELAY_H

f37858a7b14d2adb1f36d7d66d2de2cba7de820d
abdellah alaouchiche <abdou.sam7@gmail.com>
2021-03-01 11:53:36 +0100

    src/interrupt.c fix timers declaration void void -> void

--- src/interrupt.c
+++ src/interrupt.c
@@ -31,7 +31,7 @@ void __attribute__((weak)) timer_2_callback(void)
 {
 }
 
-void void __attribute__((vector(_TIMER_2_VECTOR), interrupt(ipltmr2AUTO),
+void __attribute__((vector(_TIMER_2_VECTOR), interrupt(ipltmr2AUTO),
                          nomips16)) _timer2_interrupt(void)
 {
   IFSbits_TIF(2, 0) = 0;
@@ -45,7 +45,7 @@ void __attribute__((weak)) timer_3_callback(void)
 {
 }
 
-void void __attribute__((vector(_TIMER_3_VECTOR), interrupt(ipltmr3AUTO),
+void __attribute__((vector(_TIMER_3_VECTOR), interrupt(ipltmr3AUTO),
                          nomips16)) _timer3_interrupt(void)
 {
   IFSbits_TIF(3, 0) = 0;
@@ -59,7 +59,7 @@ void __attribute__((weak)) timer_4_callback(void)
 {
 }
 
-void void __attribute__((vector(_TIMER_4_VECTOR), interrupt(ipltmr4AUTO),
+void __attribute__((vector(_TIMER_4_VECTOR), interrupt(ipltmr4AUTO),
                          nomips16)) _timer4_interrupt(void)
 {
   IFSbits_TIF(4, 0) = 0;
@@ -73,7 +73,7 @@ void __attribute__((weak)) timer_5_callback(void)
 {
 }
 
-void void __attribute__((vector(_TIMER_5_VECTOR), interrupt(ipltmr5AUTO),
+void __attribute__((vector(_TIMER_5_VECTOR), interrupt(ipltmr5AUTO),
                          nomips16)) _timer5_interrupt(void)
 {
   IFSbits_TIF(5, 0) = 0;
@@ -87,7 +87,7 @@ void __attribute__((weak)) timer_6_callback(void)
 {
 }
 
-void void __attribute__((vector(_TIMER_6_VECTOR), interrupt(ipltmr6AUTO),
+void __attribute__((vector(_TIMER_6_VECTOR), interrupt(ipltmr6AUTO),
                          nomips16)) _timer6_interrupt(void)
 {
   IFSbits_TIF(6, 0) = 0;
@@ -101,7 +101,7 @@ void __attribute__((weak)) timer_7_callback(void)
 {
 }
 
-void void __attribute__((vector(_TIMER_7_VECTOR), interrupt(ipltmr7AUTO),
+void __attribute__((vector(_TIMER_7_VECTOR), interrupt(ipltmr7AUTO),
                          nomips16)) _timer7_interrupt(void)
 {
   IFSbits_TIF(7, 1) = 0;
@@ -115,7 +115,7 @@ void __attribute__((weak)) timer_8_callback(void)
 {
 }
 
-void void __attribute__((vector(_TIMER_8_VECTOR), interrupt(ipltmr8AUTO),
+void __attribute__((vector(_TIMER_8_VECTOR), interrupt(ipltmr8AUTO),
                          nomips16)) _timer8_interrupt(void)
 {
   IFSbits_TIF(8, 1) = 0;
@@ -129,7 +129,7 @@ void __attribute__((weak)) timer_9_callback(void)
 {
 }
 
-void void __attribute__((vector(_TIMER_9_VECTOR), interrupt(ipltmr9AUTO),
+void __attribute__((vector(_TIMER_9_VECTOR), interrupt(ipltmr9AUTO),
                          nomips16)) _timer9_interrupt(void)
 {
   IFSbits_TIF(9, 1) = 0;

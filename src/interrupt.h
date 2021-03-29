da35a0878df20c4a1a286d3ce7289c9c8e12b7ab
abdellah alaouchiche <abdou.sam7@gmail.com>
2021-03-29 15:33:24 +0100

    src/interrupt.h add type and constants to standarize interrupts

--- src/interrupt.h
+++ src/interrupt.h
@@ -1,7 +1,33 @@
-#ifndef MICROPY_INCLUDED_PIC32BIT_INTERRUPT_H
-#define MICROPY_INCLUDED_PIC32BIT_INTERRUPT_H
+#ifndef INCLUDED_PIC32_INTERRUPT_H
+#define INCLUDED_PIC32_INTERRUPT_H
+
+#define INT_PRIO_BITLEN (7U)
+#define INT_SUBP_BITLEN (3U)
+
+#define IPC_SUBP_SHIFT_0 (0U)
+#define IPC_PRIO_SHIFT_0 (2U)
+
+#define IPC_SUBP_SHIFT_8  (8U)
+#define IPC_PRIO_SHIFT_8  (10U)
+
+#define IPC_SUBP_SHIFT_16 (16U)
+#define IPC_PRIO_SHIFT_16 (18U)
+
+#define IPC_SUBP_SHIFT_24 (24U)
+#define IPC_PRIO_SHIFT_24 (26U)
+
+typedef struct
+{
+  uint32_t volatile * const  flag;
+  uint32_t volatile * const  enable;
+  uint32_t volatile * const  prio;
+  uint32_t prioshift;
+  uint32_t subpshift;
+  uint8_t ibit;
+
+}intr_regs_t;
 
 void interrupt_init(void);
 uint32_t interrupt_tick_get(void);
 
-#endif
+#endif /* INCLUDED_PIC32_INTERRUPT_H */

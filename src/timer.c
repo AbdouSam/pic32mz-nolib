9aec11fc10307b61eb940a74886247dd7f675238
abdellah alaouchiche <abdou.sam7@gmail.com>
2021-03-30 10:11:20 +0100

    src/timer.c change priority constants name

--- src/timer.c
+++ src/timer.c
@@ -37,7 +37,7 @@ void init_timer1(uint32_t freq, timer1_prescaler_t prescaler,
   PR(1) = tmr_clk / freq / tmr1_prescale_value[prescaler];
 
   IFSbits_TIF(1, 0) = 0;
-  IPCbits_TIP(1, 1) = TIMER1_INTERRUPT_PERIORITY;
+  IPCbits_TIP(1, 1) = TIMER1_INT_PRIO;
   IPCbits_TIS(1, 1) = sub_periority;
 
   IECbits_TIE(1, 0) = 1;
@@ -60,7 +60,7 @@ void init_timer2(uint32_t freq, timers_prescaler_t prescaler,
   PR(2) = tmr_clk / freq / tmrs_prescale_value[prescaler];
 
   IFSbits_TIF(2, 0) = 0;
-  IPCbits_TIP(2, 2) = TIMER2_INTERRUPT_PERIORITY;
+  IPCbits_TIP(2, 2) = TIMER2_INT_PRIO;
   IPCbits_TIS(2, 2) = sub_periority;
 
   IECbits_TIE(2, 0) = 1;
@@ -85,7 +85,7 @@ void init_timer3(uint32_t freq, timers_prescaler_t prescaler,
   PR(3) = tmr_clk / freq / tmrs_prescale_value[prescaler];
 
   IFSbits_TIF(3, 0) = 0;
-  IPCbits_TIP(3, 3) = TIMER3_INTERRUPT_PERIORITY;
+  IPCbits_TIP(3, 3) = TIMER3_INT_PRIO;
   IPCbits_TIS(3, 3) = sub_periority;
 
   IECbits_TIE(3, 0) = 1;
@@ -110,7 +110,7 @@ void init_timer4(uint32_t freq, timers_prescaler_t prescaler,
   PR(4) = tmr_clk / freq / tmrs_prescale_value[prescaler];
 
   IFSbits_TIF(4, 0) = 0;
-  IPCbits_TIP(4, 4) = TIMER4_INTERRUPT_PERIORITY;
+  IPCbits_TIP(4, 4) = TIMER4_INT_PRIO;
   IPCbits_TIS(4, 4) = sub_periority;
 
   IECbits_TIE(4, 0) = 1;
@@ -135,7 +135,7 @@ void init_timer5(uint32_t freq, timers_prescaler_t prescaler,
   PR(5) = tmr_clk / freq / tmrs_prescale_value[prescaler];
 
   IFSbits_TIF(5, 0) = 0;
-  IPCbits_TIP(5, 6) = TIMER5_INTERRUPT_PERIORITY;
+  IPCbits_TIP(5, 6) = TIMER5_INT_PRIO;
   IPCbits_TIS(5, 6) = sub_periority;
 
   IECbits_TIE(5, 0) = 1;
@@ -160,7 +160,7 @@ void init_timer6(uint32_t freq, timers_prescaler_t prescaler,
   PR(6) = tmr_clk / freq / tmrs_prescale_value[prescaler];
 
   IFSbits_TIF(6, 0) = 0;
-  IPCbits_TIP(6, 7) = TIMER6_INTERRUPT_PERIORITY;
+  IPCbits_TIP(6, 7) = TIMER6_INT_PRIO;
   IPCbits_TIS(6, 7) = sub_periority;
 
   IECbits_TIE(6, 0) = 1;
@@ -185,7 +185,7 @@ void init_timer7(uint32_t freq, timers_prescaler_t prescaler,
   PR(7) = tmr_clk / freq / tmrs_prescale_value[prescaler];
 
   IFSbits_TIF(7, 1) = 0;
-  IPCbits_TIP(7, 8) = TIMER7_INTERRUPT_PERIORITY;
+  IPCbits_TIP(7, 8) = TIMER7_INT_PRIO;
   IPCbits_TIS(7, 8) = sub_periority;
 
   IECbits_TIE(7, 1) = 1;
@@ -210,7 +210,7 @@ void init_timer8(uint32_t freq, timers_prescaler_t prescaler,
   PR(8) = tmr_clk / freq / tmrs_prescale_value[prescaler];
 
   IFSbits_TIF(8, 1) = 0;
-  IPCbits_TIP(8, 9) = TIMER8_INTERRUPT_PERIORITY;
+  IPCbits_TIP(8, 9) = TIMER8_INT_PRIO;
   IPCbits_TIS(8, 9) = sub_periority;
 
   IECbits_TIE(8, 1) = 1;
@@ -235,7 +235,7 @@ void init_timer9(uint32_t freq, timers_prescaler_t prescaler,
   PR(9) = tmr_clk / freq / tmrs_prescale_value[prescaler];
 
   IFSbits_TIF(9, 1) = 0;
-  IPCbits_TIP(9, 10)    = TIMER9_INTERRUPT_PERIORITY;
+  IPCbits_TIP(9, 10)    = TIMER9_INT_PRIO;
   IPCbits_TIS(9, 10)    = sub_periority;
 
   IECbits_TIE(9, 1) = 1;

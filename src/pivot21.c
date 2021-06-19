22c3c56677a52fb375b4794f714ffe050a969df7
abdellah alaouchiche <abdou.sam7@gmail.com>
2021-06-19 10:43:28 +0100

    src/pivot21.c Add comments

--- src/pivot21.c
+++ src/pivot21.c
@@ -1,33 +1,38 @@
-#include "pivot21.h"
+/* we have to include libraries */
+/* Standard libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* libraries to Interface controller */
 #include "dio.h"
 #include "adc_controller.h"
 #include "system/tmr/sys_tmr.h"
 #include "rtc_controller.h"
 #include "debug.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-/* we have to include libraries */
+/* header file of the source */
+#include "pivot21.h"
+
 /*constants */
 
 /* Digital Inputs*/
-#define MAN_M       0  /* signal to turn on the manual mode*/
-#define INPOS       1  /* signal to read any obstacle*/
-#define INMOTOR     2  /* signal to read any motor problem*/
-#define INELEC      3  /* signal to read any electrical problem*/
-#define INWATER     4  /* signal to read any water level problem*/
-#define ECO         5  /* signal of the economic mode */
+#define MAN_M       (0)  /* signal to turn on the manual mode*/
+#define INPOS       (1)  /* signal to read any obstacle*/
+#define INMOTOR     (2)  /* signal to read any motor problem*/
+#define INELEC      (3)  /* signal to read any electrical problem*/
+#define INWATER     (4)  /* signal to read any water level problem*/
+#define ECO         (5)  /* signal of the economic mode */
 
 /* States of process*/
-#define STATE_INIT   0 /* the first state that the prrogram executes in the automatic mode*/
-#define STATE_NORMAL 1 /* tu use the normal mode*/
-#define STATE_START  2 /* to manage between irrigation and movement*/
-#define STATE_STOP   3 /* to stop the program*/
-#define STATE_MOV    4 /* to start moving the pivot*/
-#define STATE_IRRIG  5 /* to start watering the field*/
-#define STATE_PROB   6 /* to indicates some issues and stop the program*/
-#define STATE_MEH   (19) /* to enter the manual mode*/
+#define STATE_INIT   (0) /* the first state that the prrogram executes in the automatic mode*/
+#define STATE_NORMAL (1) /* tu use the normal mode*/
+#define STATE_START  (2) /* to manage between irrigation and movement*/
+#define STATE_STOP   (3) /* to stop the program*/
+#define STATE_MOV    (4) /* to start moving the pivot*/
+#define STATE_IRRIG  (5) /* to start watering the field*/
+#define STATE_PROB   (6) /* to indicates some issues and stop the program*/
+#define STATE_MEH    (19) /* to enter the manual mode*/
 /* #define STATE_MAN    8  to enter the manual mode*/
 /* #define STATE_ECO    4 to use the economic mode */
 
@@ -36,22 +41,22 @@
 /* #define MINUTE       (60000) csnt for one minute */
 
 /* Digital Outputs*/
-#define MOTOR_FWD   0   /* signal to turn on the motor */
-#define MOTOR_BWD   7   /* signal to turn on the motor */
-#define WATER       1   /* signal to the pompe */
-#define OUTELEC     2   /* LED that indicates problem in electricity */
-#define OUTMOTOR    3   /* LED that indicates problem in the motor */
-#define OUTWATER    4   /* LED that indicates lack in the water */
-#define OUTPOS      5   /* LED that indicates an obstacle */
-#define DIRECTION   6   /* SIGNAL that indicates the direction */
+#define MOTOR_FWD   (0)   /* signal to turn on the motor */
+#define MOTOR_BWD   (7)   /* signal to turn on the motor */
+#define WATER       (1)   /* signal to the pompe */
+#define OUTELEC     (2)   /* LED that indicates problem in electricity */
+#define OUTMOTOR    (3)   /* LED that indicates problem in the motor */
+#define OUTWATER    (4)   /* LED that indicates lack in the water */
+#define OUTPOS      (5)   /* LED that indicates an obstacle */
+#define DIRECTION   (6)   /* SIGNAL that indicates the direction */
 
 
 /* Analog Inputs*/
-#define TEMP        0     /* reading from temperature sensure */
-#define HUM         1     /* reading from humidity sensure */
+#define TEMP        (0)     /* reading from temperature sensure */
+#define HUM         (1)     /* reading from humidity sensure */
 
-#define DIR_FWD          0     /* reading from humidity sensure */
-#define DIR_BWD          1     /* reading from humidity sensure */
+#define DIR_FWD          (0)     /* reading from humidity sensure */
+#define DIR_BWD          (1)     /* reading from humidity sensure */
 
 
 /* Default values for timing */
@@ -63,7 +68,19 @@
 
 /* add 2 more relays for marche avant , arriere*/
 
-/* variable set*/
+/***********************************************
+ * Private Data Type
+ ***********************************************/
+
+typedef struct
+{
+  int start;  
+  int size;  
+}parse_data_t;
+
+/***********************************************
+ * Private Data - Variables -
+ ***********************************************/
 
 static int state;
 static int temp;
@@ -94,19 +111,150 @@ static int motor_direction = DIR_FWD;
 /* static bool start_timer_mov = true ; this should be connected to a bool pin "once f denia used"*/
 
 /***********************************************
- * 
+ * Private Functions
  ***********************************************/
 
-typedef struct
-{
-  int start;  
-  int size;  
-}parse_data_t;
-
-void set_state (int st)
+static void set_state (int st)
 {
   state = st;
   state_entery = true;
+}
+
+/* functions */
+
+static bool is_weather_valid(void)
+{
+  return ((hum < 50 && temp < 27 ) || (hum < 20 && temp > 40 ));
+}
+
+static bool is_time_valid(void)
+{
+  return (time_cond <= time_on);
+}
+static bool is_eco_time(void)
+{
+  return (curr_hr >= 22 || curr_hr <= 5);
+}
+static bool check_failures(void)
+{
+  return ( /* ppos == true || */ pmotor == true || pwater == true || pelec == true);
+}
+
+static void irrig_done ( uintptr_t context, uint32_t currTick )
+{
+  dio_turnoff(WATER) ;
+  set_state(STATE_MOV);
+  app_dbg_msg("I am done irrigating! \n ");
+
+}
+
+static void disp_done ( uintptr_t context, uint32_t currTick )
+{ 
+   dio_turnoff(MOTOR_FWD) ;
+   dio_turnoff(MOTOR_BWD) ;
+   set_state(STATE_IRRIG);
+   app_dbg_msg("I am done moving! \n ");
+}
+
+static void ppos_done(uintptr_t context, uint32_t cuurTick)
+{
+  /* Recheck ppos after 5 seconds */
+  if (ppos)
+  {
+     new_ppos = true;
+
+     if (motor_direction == DIR_FWD)
+        { 
+          app_dbg_msg("Position problem confirmed, Turning Backward.\n");
+          dio_turnoff(MOTOR_FWD);
+          dio_turnon(MOTOR_BWD);
+        }
+      else
+        {
+          app_dbg_msg("Position problem confirmed, Turning Forward.\n");
+          dio_turnoff(MOTOR_BWD);
+          dio_turnon(MOTOR_FWD);
+        }
+      
+        motor_direction = (motor_direction == DIR_FWD) ? DIR_BWD : DIR_FWD;
+  }
+
+}
+/*
+void stp_timer_cb( uintptr_t context, uint32_t currTick )
+{
+  set_state(STATE_STOP);
+  app_dbg_msg("Time is up \n ");
+}
+*/
+
+static void check_water()
+{
+  if (pwater == true )
+  {
+    dio_turnon(OUTWATER) ;
+    app_dbg_msg("Water problem \n ");
+  }
+  else {
+    dio_turnoff(OUTWATER);
+  }
+}
+
+static void check_motor()
+{
+  if (pmotor == true)
+  {
+    dio_turnon(OUTMOTOR) ;
+    app_dbg_msg("MOTOR problem \n ");
+  }
+  else {
+    dio_turnoff(OUTMOTOR);
+
+  }
+}
+
+static void check_elec()
+{
+  if ( pelec == true)
+  {
+    dio_turnon(OUTELEC) ;
+    app_dbg_msg("Elec problem \n ");
+  }
+  else
+  {
+    dio_turnoff(OUTELEC);
+  }
+}
+
+static void check_pos()
+{
+  if (ppos == true )
+  {
+    dio_turnon(OUTPOS) ;
+    app_dbg_msg("obstacle\n ");
+  }
+  else
+  {
+    dio_turnoff(OUTPOS);
+  }
+}
+
+static void gethum (void)
+{
+  uint16_t Dhum ;
+
+  /* Read humidity*/
+  adc_controller_readadc(HUM, &Dhum);
+  hum = Dhum * 100 / 4095 ;
+}
+
+static void gettemp(void)
+{
+  uint16_t Dtemp ;
+
+  /* Read temperature*/
+  adc_controller_readadc(TEMP, &Dtemp);
+  temp = (Dtemp * TMAX / 4095) + TMIN ;
 }
 
 int pivot21_set_parameter(const char *frame)
@@ -337,141 +485,6 @@ int pivot21_read_parameter(const char *frame, char *retframe)
   }
 
   return 0;
-}
-
-/* functions */
-
-static bool is_weather_valid(void)
-{
-  return ((hum < 50 && temp < 27 ) || (hum < 20 && temp > 40 ));
-}
-
-static bool is_time_valid(void)
-{
-  return (time_cond <= time_on);
-}
-static bool is_eco_time(void)
-{
-  return (curr_hr >= 22 || curr_hr <= 5);
-}
-static bool check_failures(void)
-{
-  return ( /* ppos == true || */ pmotor == true || pwater == true || pelec == true);
-}
-
-void irrig_done ( uintptr_t context, uint32_t currTick )
-{
-  dio_turnoff(WATER) ;
-  set_state(STATE_MOV);
-  app_dbg_msg("I am done irrigating! \n ");
-
-}
-void disp_done ( uintptr_t context, uint32_t currTick )
-{ 
-   dio_turnoff(MOTOR_FWD) ;
-   dio_turnoff(MOTOR_BWD) ;
-   set_state(STATE_IRRIG);
-   app_dbg_msg("I am done moving! \n ");
-}
-
-void ppos_done(uintptr_t context, uint32_t cuurTick)
-{
-  /* Recheck ppos after 5 seconds */
-  if (ppos)
-  {
-     new_ppos = true;
-
-     if (motor_direction == DIR_FWD)
-        { 
-          app_dbg_msg("Position problem confirmed, Turning Backward.\n");
-          dio_turnoff(MOTOR_FWD);
-          dio_turnon(MOTOR_BWD);
-        }
-      else
-        {
-          app_dbg_msg("Position problem confirmed, Turning Forward.\n");
-          dio_turnoff(MOTOR_BWD);
-          dio_turnon(MOTOR_FWD);
-        }
-      
-        motor_direction = (motor_direction == DIR_FWD) ? DIR_BWD : DIR_FWD;
-  }
-
-}
-/*
-void stp_timer_cb( uintptr_t context, uint32_t currTick )
-{
-  set_state(STATE_STOP);
-  app_dbg_msg("Time is up \n ");
-}
-*/
-void check_water()
-{
-  if (pwater == true )
-  {
-    dio_turnon(OUTWATER) ;
-    app_dbg_msg("Water problem \n ");
-  }
-  else {
-    dio_turnoff(OUTWATER);
-  }
-}
-
-void check_motor()
-{
-  if (pmotor == true)
-  {
-    dio_turnon(OUTMOTOR) ;
-    app_dbg_msg("MOTOR problem \n ");
-  }
-  else {
-    dio_turnoff(OUTMOTOR);
-
-  }
-}
-
-void check_elec()
-{
-  if ( pelec == true)
-  {
-    dio_turnon(OUTELEC) ;
-    app_dbg_msg("Elec problem \n ");
-  }
-  else
-  {
-    dio_turnoff(OUTELEC);
-  }
-}
-
-void check_pos()
-{
-  if (ppos == true )
-  {
-    dio_turnon(OUTPOS) ;
-    app_dbg_msg("obstacle\n ");
-  }
-  else
-  {
-    dio_turnoff(OUTPOS);
-  }
-}
-
-void gethum (void)
-{
-  uint16_t Dhum ;
-
-  /* Read humidity*/
-  adc_controller_readadc(HUM, &Dhum);
-  hum = Dhum * 100 / 4095 ;
-}
-
-void gettemp(void)
-{
-  uint16_t Dtemp ;
-
-  /* Read temperature*/
-  adc_controller_readadc(TEMP, &Dtemp);
-  temp = (Dtemp * TMAX / 4095) + TMIN ;
 }
 
 void pivot21_init(void)
